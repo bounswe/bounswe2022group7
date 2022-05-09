4abce50e00bd12cb54167b719aa029d1352c2630
CahidArda <57228345+CahidArda@users.noreply.github.com>
2022-05-09 13:27:05 +0300

    Add views to __init__.py (#157)

--- practice-app/website/__init__.py
+++ practice-app/website/__init__.py
@@ -20,6 +20,10 @@ def create_app():
 
     db.init_app(app)
 
+    from .views import views
+
+    app.register_blueprint(views, url_prefix="/")
+
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
