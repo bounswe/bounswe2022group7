0c4a9c773ee3c21f87634964dc76de504bb19aa1
guneyizol <guneyizol@gmail.com>
2022-05-09 17:25:14 +0300

    Disallow logging in when already logged in

--- practice-app/website/auth.py
+++ practice-app/website/auth.py
@@ -1,11 +1,12 @@
 from flask import Blueprint, jsonify, request
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import login_user, login_required, logout_user, current_user
+from flask_login import login_user, login_required, logout_user, current_user, user_logged_out
 from . import db
 from .models import User
 
 
 auth = Blueprint('auth', __name__)
+
 
 @auth.route('/signup', methods=['POST'])
 def signup():
@@ -18,7 +19,7 @@ def signup():
     user = User.query.filter_by(email=email).first()
     if user:
         return {'error': 'This email is already taken.'}, 409
-    
+
     new_user = User(
         email=email,
         password=generate_password_hash(password, method='sha256'),
@@ -36,21 +37,26 @@ def signup():
         'is_verified': new_user.is_verified
     }, 201
 
+
 @auth.route('/login', methods=['POST'])
 def login():
-    data = request.get_json()
-    email = data['email']
-    password = data['password']
-    remember = data['remember']
+    if not current_user.is_authenticated:
+        data = request.get_json()
+        email = data['email']
+        password = data['password']
+        remember = data['remember']
 
-    user = User.query.filter_by(email=email).first()
-    if not user or not check_password_hash(user.password, password):
-        return {'error': 'Incorrect email or password.'}, 401
-    
-    login_user(user, remember=remember)
-    return {
-        'email': user.email,
-    }, 200
+        user = User.query.filter_by(email=email).first()
+        if not user or not check_password_hash(user.password, password):
+            return {'error': 'Incorrect email or password.'}, 401
+
+        login_user(user, remember=remember)
+        return {
+            'email': user.email,
+        }, 200
+    else:
+        return {'error': 'already logged in'}, 405
+
 
 @auth.route('/logout')
 @login_required
