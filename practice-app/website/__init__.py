d10514ff2a9274bd7370de7f7073172f3a33dde9
CahidArda <57228345+CahidArda@users.noreply.github.com>
2022-05-09 17:06:17 +0300

    Create API endpoints for viewing and creating events (#158)

--- practice-app/website/__init__.py
+++ practice-app/website/__init__.py
@@ -21,8 +21,10 @@ def create_app():
     db.init_app(app)
 
     from .views import views
+    from .api.event import event
 
     app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(event, url_prefix="/api/")
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
