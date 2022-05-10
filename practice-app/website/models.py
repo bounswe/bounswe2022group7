20dc44e182691b03ecb5e75dee54badfa8dfdd29
guneyizol <guneyizol@gmail.com>
2022-05-11 00:44:56 +0300

    Change session-based auth to token-based

--- practice-app/website/models.py
+++ practice-app/website/models.py
@@ -1,8 +1,7 @@
 from . import db
-from flask_login import UserMixin
 
 
-class User(db.Model, UserMixin):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
