80a4aef6a0d77d4e7ae11db3ef805d27b8d9233d
CahidArda <57228345+CahidArda@users.noreply.github.com>
2022-05-12 12:07:15 +0300

    Add API endpoint for home & search (#166)

--- practice-app/website/__init__.py
+++ practice-app/website/__init__.py
@@ -34,9 +34,11 @@ def create_app():
 
     from .views import views
     from .api.event import event
+    from .api.home import home
 
     app.register_blueprint(views, url_prefix="/")
-    app.register_blueprint(event, url_prefix="/api/")
+    app.register_blueprint(home, url_prefix="/api")
+    app.register_blueprint(event, url_prefix="/api")
 
     from .auth import auth
     app.register_blueprint(auth, url_prefix="/api/")
