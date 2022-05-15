afed7d28b2007edf5870fecf2b9a193b49d4be4c
CahidArda <cahidardaooz@hotmail.com>
2022-05-16 02:08:23 +0300

    Update event and views with new token design (#179)

--- practice-app/website/views.py
+++ practice-app/website/views.py
@@ -1,23 +1,26 @@
-from pickletools import read_unicodestring1
-from flask import Blueprint, render_template
-from flask_jwt_extended import jwt_required
+from flask import Blueprint, render_template, request
+
+from .jwt import artist_token_required, user_token_required
 
 views = Blueprint('views', __name__)
 
 
 @views.route("/")
 def home():
-    return render_template("home.html")
+    token = request.args.get("token")
+    return render_template("home.html", token=token)
 
 
 @views.route("event/<event_id>/")
 def view_event(event_id):
-    return render_template("view_event.html", event_id=event_id)
-
+    token = request.args.get("token")
+    return render_template("view_event.html", event_id=event_id, token=token)
 
 @views.route("create_event/")
+@artist_token_required
 def create_event():
-    return render_template("create_event.html")
+    token = request.args.get("token")
+    return render_template("create_event.html", token=token)
 
 
 @views.route('signup/')
