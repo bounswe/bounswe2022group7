ad367397eb21e8b9e823d1141373a1f9636d832a
atillaturkmen <turkmenatilla522@gmail.com>
2022-05-15 21:15:11 +0300

    Added unit tests for wikipedia search

--- practice-app/website/api/wikipedia.py
+++ practice-app/website/api/wikipedia.py
@@ -14,6 +14,9 @@ def wikipedia_definition():
     if "word" not in req_json:
         return {"error": "You did not send any words."}, 400
 
+    if req_json["word"] == "":
+        return {"error": "You did not send any words."}, 400
+
     word = req_json["word"]
 
     wikipediaEndPoint = "https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exlimit=1&explaintext=1&exsectionformat=plain&format=json&origin=*&titles=" + word
@@ -22,8 +25,5 @@ def wikipedia_definition():
     res = json.loads(urllib.request.urlopen(req).read())
     pages = res["query"]["pages"]
     definition = pages[list(pages.keys())[0]]["extract"]
-    print(definition)
 
     return {"definition": definition}, 200
-
-# METHODS
