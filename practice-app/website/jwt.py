bc0e2bba80f70d1afbc0a7f0e54a42d689f6c95c
CahidArda <cahidardaooz@hotmail.com>
2022-05-16 15:17:55 +0300

    Manage tokens with sessions (#179)

--- practice-app/website/jwt.py
+++ practice-app/website/jwt.py
@@ -1,11 +1,35 @@
 from functools import wraps
-from flask import request
+from flask import Blueprint, request, session
 from flask_jwt_extended import decode_token
+
+# Token Blueprint
+
+token = Blueprint("token", __name__)
+
+@token.route("/set", methods=["POST"])
+def set_session_token():
+    token = request.json["token"]
+    if token:
+        session["token"] = token
+        return {"success": "Token succesfully set."}, 200
+    else:
+        return {"success": "A token should be provided in the request body."}, 400
+
+@token.route("drop", methods=["POST"])
+def drop_session_token():
+    current_token = session["token"] 
+    if current_token:
+        session["token"] = None
+        return {"success": "Token removed from session."}, 200
+    else:
+        return {"error": "There is no token to remove from the session."}, 404
+
+# Decorators
 
 def artist_token_required(func):
     @wraps(func)
     def wrapped(*args, **kwargs):
-        token = request.args.get("token")
+        token = session["token"]
         if not token:
             return {"error": "You can not view this page without providing a valid token."}, 403
         try:
@@ -20,7 +44,7 @@ def artist_token_required(func):
 def user_token_required(func):
     @wraps(func)
     def wrapped(*args, **kwargs):
-        token = request.args.get("token")
+        token = session["token"]
         if not token:
             return {"error": "You can not view this page without providing a valid token."}, 403
         try:
