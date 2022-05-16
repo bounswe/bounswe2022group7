b85fa1e966d552805becf02ffb947c0abaf938db
CahidArda <cahidardaooz@hotmail.com>
2022-05-16 15:22:41 +0300

    Remove unused parameters

--- practice-app/website/views.py
+++ practice-app/website/views.py
@@ -7,20 +7,17 @@ views = Blueprint('views', __name__)
 
 @views.route("/")
 def home():
-    token = request.args.get("token")
-    return render_template("home.html", token=token)
+    return render_template("home.html")
 
 
 @views.route("event/<event_id>/")
 def view_event(event_id):
-    token = request.args.get("token")
-    return render_template("view_event.html", event_id=event_id, token=token)
+    return render_template("view_event.html", event_id=event_id)
 
 @views.route("create_event/")
 @artist_token_required
 def create_event():
-    token = request.args.get("token")
-    return render_template("create_event.html", token=token)
+    return render_template("create_event.html")
 
 
 @views.route('signup/')
