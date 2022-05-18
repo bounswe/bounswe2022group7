c1213f7405467a8981a49377bbb29f1916df5cdc
atillaturkmen <turkmenatilla522@gmail.com>
2022-05-18 11:49:55 +0300

    unit test for viewing profile

---
+++ practice-app/website/test/test_profile.py
@@ -0,0 +1,44 @@
+import unittest
+
+from .. import create_app, db
+
+from ..api.profile import *
+
+TEST_DB_NAME = "test_database.db"
+
+class TestProfile(unittest.TestCase):
+
+    def setUp(self):
+        
+        app = create_app(TEST_DB_NAME)
+        self.ctx = app.app_context()
+        self.ctx.push()
+        self.client = app.test_client()
+
+        self.user_data = {
+            "email": "user@mail.com",
+            "password": "pass",
+            "first_name": "UserName",
+            "last_name": "Test",
+            "is_artist": None
+        }
+
+        response = self.client.post("/api/signup",
+                                    json=self.user_data,
+                                    content_type = "application/json; charset=UTF-8")
+        self.user_access_token = response.json["access_token"]
+        self.assertEqual(response.status, "201 CREATED")
+
+    def test_profile(self):
+        response = self.client.get("/api/profile/",
+                                content_type="application/json; charset=UTF-8",
+                                headers = {"Authorization": "Bearer %s" % self.user_access_token})
+        self.assertEqual(response.status, "200 OK")
+        data = response.json
+        self.assertEqual(data["email"], self.user_data["email"])
+        self.assertEqual(data["first_name"], self.user_data["first_name"])
+        self.assertEqual(data["last_name"], self.user_data["last_name"])
+
+    def tearDown(self):
+        db.drop_all()
+        self.ctx.pop()
