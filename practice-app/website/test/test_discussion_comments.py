c1d70199d286c151491ca7ef9d284751d9359943
azizamankenova <azizamankenova@gmail.com>
2022-05-19 18:57:37 +0300

    Completed the unittests for forum post comments (#163)

--- practice-app/website/test/test_discussion_comments.py
+++ practice-app/website/test/test_discussion_comments.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest import mock
+# from unittest import mock
 from ..api.discussion_forum import translate
 
 from .. import create_app, db
@@ -12,30 +12,47 @@ class TestPostComment(unittest.TestCase):
 
     def setUp(self):
 
-        app = create_app()
+        app = create_app(testing=True)
         self.ctx = app.app_context()
         self.ctx.push()
         self.client = app.test_client()
 
+        self.artist_data = {
+            "email": "artist@mail.com",
+            "password": "pass",
+            "first_name": "ArtistName",
+            "last_name": "Test",
+            "is_artist": "on"
+        }
+
+        self.user_data = {
+            "email": "user@mail.com",
+            "password": "pass",
+            "first_name": "UserName",
+            "last_name": "Test",
+            "is_artist": None
+        }
+
+
         self.correct_data = [
             {
                 "parent_post": "1",
-                "text": "Hi",
+                "text": "Comment 1",
+                "content_uri": "https://random.imagecdn.app/500/150"
+            },
+            {
+                "parent_post": "2",
+                "text": "Comment 2",
                 "content_uri": "https://random.imagecdn.app/500/150"
             },
             {
                 "parent_post": "1",
-                "text": "Hi",
-                "content_uri": "https://random.imagecdn.app/500/150"
+                "text": "Empty content URI",
+                "content_uri": ""
             },
             {
                 "parent_post": "1",
-                "text": "Hi",
-                "content_uri": "https://random.imagecdn.app/500/150"
-            },
-            {
-                "parent_post": "1",
-                "text": "Hi",
+                "text": "Comment 4",
                 "content_uri": "https://random.imagecdn.app/500/150"
             },
             {
@@ -55,48 +72,71 @@ class TestPostComment(unittest.TestCase):
             "text": "Hi",
             "content_uri": "https://random.imagecdn.app/500/150"
         }
+        self.post_data = {
+            "title": "standard_sample",
+            "description": "description1",
+            "content_uri": "https://www.hepsiburada.com/hayatburada/wp-content/uploads/2021/10/shutterstock_1024133086.jpg"
+        }
+
+        response = self.client.post("/api/signup",
+                                    json=self.artist_data,
+                                    content_type = "application/json; charset=UTF-8")
+        self.artist_access_token = response.json["access_token"]
+        self.assertEqual(response.status, "201 CREATED")
+
+        response = self.client.post("/api/signup",
+                                    json=self.user_data,
+                                    content_type = "application/json; charset=UTF-8")
+        self.user_access_token = response.json["access_token"]
+        self.assertEqual(response.status, "201 CREATED")
+
+        post_response = self.client.post("/api/forum_post/", json=self.post_data, content_type="application/json; charset=UTF-8", 
+                                headers = {"Authorization": "Bearer " + self.user_access_token})
+        self.assertEqual(response.status, "201 CREATED")
+        self.post_id = post_response.json["id"]
 
         self.comment_ids = []
         for json in self.correct_data:
-            response = self.make_create_post_comment_request(json)
+            response = self.make_create_post_comment_request(json, self.user_access_token)
 
             self.assertEqual(response.status, "201 CREATED")
             self.comment_ids.append(response.json["id"])
 
     # This method will be used by the mock to replace requests.get
-    def mocked_request_external(*args, **kwargs):
-        class MockResponse:
-            def __init__(self, json_data, status_code):
-                self.json_data = json_data
-                self.status_code = status_code
+    # def mocked_request_external(*args, **kwargs):
+    #     class MockResponse:
+    #         def __init__(self, json_data, status_code):
+    #             self.json_data = json_data
+    #             self.status_code = status_code
 
-            def json(self):
-                return self.json_data
+    #         def json(self):
+    #             return self.json_data
 
-        if args[0] == 'https://deep-translate1.p.rapidapi.com/language/translate/v2':
-            return MockResponse(json.stringify({
-                "data": {
-                    "translations": {
-                        "translatedText": "Merhaba"
-                    }
-                }
-            }), 200)
+    #     if args[0] == 'https://deep-translate1.p.rapidapi.com/language/translate/v2':
+    #         return MockResponse(json.stringify({
+    #             "data": {
+    #                 "translations": {
+    #                     "translatedText": "Merhaba"
+    #                 }
+    #             }
+    #         }), 200)
 
-        return MockResponse(None, 404)
+    #     return MockResponse(None, 404)
 
-    def make_create_post_comment_request(self, json):
+    def make_create_post_comment_request(self, json, token):
         return self.client.post("/api/forum_comment_post",
                                 json=json,
-                                content_type="application/json; charset=UTF-8")
+                                content_type="application/json; charset=UTF-8",
+                                headers = {"Authorization": "Bearer %s" % token})
 
     def make_view_discussion_post_request(self, post_id):
-        return self.client.get(f"/api//get_discussion_post/{post_id}")
+        return self.client.get(f"/api/get_discussion_post/{post_id}")
 
     # test create_event API endpoint
 
-    @mock.patch('api.discussion_forum.translate.request', side_effect=mocked_request_external)
-    def test_create_post_comment_endpoint(self):
-        response = self.make_create_post_comment_request(self.test_data)
+    # @mock.patch('api.discussion_forum.translate.request', side_effect=mocked_request_external)
+    def test_create_post_comment_endpoint_user(self):
+        response = self.make_create_post_comment_request(self.test_data, self.user_access_token)
         
         self.assertEqual(response.status, "201 CREATED")
         self.assertEqual(response.json["id"], self.comment_ids[-1] + 1)
@@ -104,71 +144,63 @@ class TestPostComment(unittest.TestCase):
         self.assertIsNotNone(PostComment.query.get(response.json["id"]))
         self.assertIsNone(PostComment.query.get(response.json["id"] + 1))
 
-    # def test_create_duplicate_event(self):
-    #     response = self.make_create_post_comment_request(self.correct_data[0])
-    #     self.assertEqual(response.status, "409 CONFLICT")
-
-    # @mock.patch('api.discussion_forum.translate.request', side_effect=mocked_request_external)
-    # def test_missing_field(self):
-    #     keys = ["parent_post", "text"]
-    #     for key in keys:
-    #         value = self.test_data.pop(key)
-    #         response = self.make_create_post_comment_request(self.test_data)
-    #         self.test_data[key] = value
-
-    #         self.assertEqual(response.status, "400 BAD REQUEST")
-    #         self.assertTrue("error" in response.json)
-    #         self.assertEqual(response.json["error"], "There was an error on key / value pairs on request body.")
-
-    # @mock.patch('api.discussion_forum.translate.request', side_effect=mocked_request_external)
-    # def test_empty_parent_post(self):
-    #     value = self.test_data.pop("parent_post")
-    #     self.test_data["parent_post"] = ""
-    #     response = self.make_create_post_comment_request(self.test_data)
-    #     self.test_data["parent_post"] = value
-    #     self.assertEqual(response.status, "400 BAD REQUEST")
-    #     self.assertTrue("error" in response.json)
-    #     self.assertEqual(response.json["error"], "Parent post id is missing")
-
-
-
-    # def test_date_format(self):
+    def test_create_post_comment_endpoint_artist(self):
+        response = self.make_create_post_comment_request(self.test_data, self.artist_access_token)
         
-    #     wrong_dates = ["12-04-2007",
-    #                    " 2005-04-03  ",
-    #                    "2001.03.10",
-    #                    "12.04.2007",
-    #                    "Hello World!",
-    #                    1234,
-    #                    True]
+        self.assertEqual(response.status, "201 CREATED")
+        self.assertEqual(response.json["id"], self.comment_ids[-1] + 1)
 
-    #     correct_date = self.test_data["date"]
-    #     for date in wrong_dates:
-    #         self.test_data["date"] = date
-    #         response = self.make_create_post_comment_request(self.test_data)
-    #         self.assertEqual(response.status, "400 BAD REQUEST")
-    #         self.assertTrue("error" in response.json)
-    #         self.assertEqual(response.json["error"], f"Date you have entered is not valid. Format is \"%Y-%m-%d\". You entered \"{date}\"." )
-            
-    # # test view_event API endpoint
+        self.assertIsNotNone(PostComment.query.get(response.json["id"]))
+        self.assertIsNone(PostComment.query.get(response.json["id"] + 1))
 
-    # def test_view_existing_event(self):
-    #     response = self.make_view_event_request(self.comment_ids[0])
-    #     self.assertEqual(response.status, "200 OK")
 
-    # def test_view_nonexisting_event(self):
-    #     response = self.make_view_event_request(self.comment_ids[-1] + 1)
-    #     self.assertEqual(response.status, "404 NOT FOUND")
+    # @mock.patch('api.discussion_forum.translate.request', side_effect=mocked_request_external)
+    def test_missing_field(self):
+        # keys = ["parent_post", "text"]
+        keys = list(self.test_data.keys()).copy()
+        for key in keys:
+            value = self.test_data.pop(key)
+            response = self.make_create_post_comment_request(self.test_data, self.user_access_token)
+            self.test_data[key] = value
 
-    # # test event helper methods
+            self.assertEqual(response.status, "400 BAD REQUEST")
+            self.assertTrue("error" in response.json)
+            self.assertEqual(response.json["error"], "There was an error on key / value pairs on request body.")
 
-    # def test_title_exists(self):
-    #     self.assertTrue(title_exists("Title 1"))
-    #     self.assertFalse(title_exists("Title That Doesn't Exist"))
+    # @mock.patch('api.discussion_forum.translate.request', side_effect=mocked_request_external)
+    def test_empty_parent_post(self):
+        value = self.test_data.pop("parent_post")
+        self.test_data["parent_post"] = ""
+        response = self.make_create_post_comment_request(self.test_data, self.user_access_token)
+        self.test_data["parent_post"] = value
+        self.assertEqual(response.status, "400 BAD REQUEST")
+        self.assertTrue("error" in response.json)
+        self.assertEqual(response.json["error"], "Parent post id is empty")
 
-    # def test_date_valid(self):
-        # self.assertFalse(date_valid("2014-14-22"))
-        # self.assertTrue(date_valid("1020-4-14"))
+    def test_empty_body_text(self):
+        value = self.test_data.pop("text")
+        self.test_data["text"] = ""
+        response = self.make_create_post_comment_request(self.test_data, self.user_access_token)
+        self.test_data["text"] = value
+        self.assertEqual(response.status, "400 BAD REQUEST")
+        self.assertTrue("error" in response.json)
+        self.assertEqual(response.json["error"], "You have not provided body of the comment text")
+
+
+    def test_authorization(self):
+        response = self.client.post("/api/forum_comment_post",
+                                json=self.test_data,
+                                content_type="application/json; charset=UTF-8")
+        self.assertEqual(response.status, "401 UNAUTHORIZED")
+    
+    def test_view_existing_forum_post(self):
+        response = self.make_view_discussion_post_request(self.comment_ids[0])
+        self.assertEqual(response.status, "200 OK")
+
+    def test_view_nonexisting_forum_post(self):
+        response = self.make_view_discussion_post_request(self.comment_ids[-1] + 1)
+        self.assertEqual(response.status, "404 NOT FOUND")
+
 
     def tearDown(self):
         db.drop_all()
