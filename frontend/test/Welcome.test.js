cbcb1fad45000d1738a341695d11e21a18a6becb
guneyizol <guneyizol@gmail.com>
2022-10-10 21:50:10 +0300

    Refactor test

--- frontend/test/Welcome.test.js
+++ frontend/test/Welcome.test.js
@@ -4,15 +4,25 @@ import { act } from 'react-dom/test-utils'
 import { Welcome } from '../src/Welcome'
 
 describe('Welcome', () => {
-    it('renders a greeting', () => {
-        const name = 'guney'
-        const component = <Welcome name={name} id="welcome" />
-        const container = document.createElement('div')
-        document.body.appendChild(container)
+    let container
 
+    beforeEach(() => {
+        container = document.createElement('div')
+        document.body.replaceChildren(container)
+
+    })
+
+    const render = component => {
         act(() =>
             ReactDOM.createRoot(container).render(component)
         )
+        
+    }
+
+    it('renders a greeting', () => {
+        const name = 'guney'
+
+        render(<Welcome name={name} id="welcome" />)
 
         expect(document.querySelector('#welcome').textContent)
             .toContain('Hello, guney')
