ef16183d85dee47cab88820143b9a46ee0e5269d
bnbcfyh <56476673+bnbcfyh@users.noreply.github.com>
2022-10-18 02:06:48 +0300

    Add drawer

--- android/lib/pages/home_page.dart
+++ android/lib/pages/home_page.dart
@@ -34,7 +34,6 @@ class HomePage extends StatelessWidget {
                 Navigator.pushNamed(context, '/search');
               },
             ),
-
             IconButton(
               icon: const Icon(Icons.account_circle),
               iconSize: 30.0,
@@ -43,8 +42,6 @@ class HomePage extends StatelessWidget {
                 Navigator.pushNamed(context, '/profile');
               },
             ),
-
-
           ],
         ),
         SliverList(
@@ -57,6 +54,73 @@ class HomePage extends StatelessWidget {
           childCount: artItems.length,
         )),
       ]),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage(
+                      'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Vincent_Van_Gogh_-_Three_Sunflowers_F453.jpg/1280px-Vincent_Van_Gogh_-_Three_Sunflowers_F453.jpg'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: Stack(
+                alignment: Alignment.bottomLeft,
+                children: [
+                  Text(
+                    "Art Community",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text("Home"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text("Profile"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.photo_library),
+              title: Text("Online Galleries"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.museum),
+              title: Text("Exhibitions"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.attach_money),
+              title: Text("Art Item Auctions"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.forum),
+              title: Text("Discussion Page"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text("Settings"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.logout),
+              title: Text("Logout"),
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
       bottomNavigationBar: BottomNavigationBar(
         items: const [
           BottomNavigationBarItem(
@@ -77,16 +141,14 @@ class HomePage extends StatelessWidget {
         currentIndex: 0,
         onTap: (index) {
           if (index == 0) {
-            Navigator.pushNamed(context, '/home');
+            //Navigator.pushNamed(context, '/home');
           } else if (index == 1) {
-            Navigator.pushNamed(context, '/events');
+            //Navigator.pushNamed(context, '/events');
           } else if (index == 2) {
-            Navigator.pushNamed(context, '/discussionPage');
+            //Navigator.pushNamed(context, '/discussionPage');
           }
         },
       ),
     );
   }
 }
-
-
