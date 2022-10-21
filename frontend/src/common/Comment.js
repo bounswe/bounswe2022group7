cec0f31307f208fe43c5ecfd56c5ab8003923a45
CahidArda <57228345+CahidArda@users.noreply.github.com>
2022-10-21 20:29:53 +0300

    Add menu and like button to comments (FE, #270)

--- frontend/src/common/Comment.js
+++ frontend/src/common/Comment.js
@@ -5,12 +5,19 @@ import Card from '@mui/material/Card';
 import { CardContent, CardHeader } from '@mui/material';
 import Typography from '@mui/material/Typography';
 
+import IconButton from '@mui/material/IconButton';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
+import CardActions from '@mui/material/CardActions';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+
 function Comment(title, subheader, commentBody) {
     return (
       <Card sx={{
         width: '%90',
         padding: 2
       }}>
+        
         <CardHeader 
           avatar={
             <Avatar sx={{ bgcolor: 'secondary.main' }} aria-label="user">
@@ -19,13 +26,25 @@ function Comment(title, subheader, commentBody) {
           }
           title={title}
           subheader={subheader}
+          action={
+            <IconButton aria-label="settings">
+              <MoreVertIcon />
+            </IconButton>
+          }
         />
+
         <CardContent>
           <Typography variant="body2" color="text.secondary">
             {commentBody}
           </Typography>
         </CardContent>
-  
+
+        <CardActions disableSpacing>
+          <IconButton aria-label="add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+        </CardActions>
+
       </Card>
     )
   }
