d3dc28ef7f91d496281bf24e8dcd22f91ba8adb2
musasimsekdotdll <musa.simsek@boun.edu.tr>
2022-10-22 16:38:35 +0300

    username must be added

--- android/lib/models/user_model.dart
+++ android/lib/models/user_model.dart
@@ -2,6 +2,7 @@ class User {
   final String name;
   final String email;
   final String imageUrl;
+  final String username;
 
-  User({required this.name, required this.email, required this.imageUrl});
+  User({required this.name, required this.email, required this.imageUrl, required this.username,});
 }
