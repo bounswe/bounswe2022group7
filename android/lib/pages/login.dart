96fb4484758e6a83f198f960d8c04ac3a64dbd4c
AlicanM <alicanmilani@hotmail.com>
2022-10-23 22:36:19 +0300

    UI of the login page is created

--- android/lib/pages/login.dart
+++ android/lib/pages/login.dart
@@ -11,20 +11,127 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  Widget usernameField = Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 25.0),
+    child: Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(color: Colors.black),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: const Padding(
+        padding: EdgeInsets.only(left: 20.0),
+        child: TextField(
+          autofocus: false,
+          decoration: InputDecoration(
+            border: InputBorder.none,
+            hintText: 'Username',
+          ),
+        ),
+      ),
+    ),
+  );
+
+  Widget passwordField = Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 25.0),
+    child: Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(color: Colors.black),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: const Padding(
+        padding: EdgeInsets.only(left: 20.0),
+        child: TextField(
+          autofocus: false,
+          obscureText: true,
+          decoration: InputDecoration(
+            border: InputBorder.none,
+            hintText: 'Password',
+          ),
+        ),
+      ),
+    ),
+  );
+
+  Widget signInButton = Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 25.0),
+    child: Container(
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.indigo,
+        border: Border.all(color: Colors.black),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: const Center(
+        child: Text(
+          'Log In',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+          ),
+        ),
+      ),
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: FormAppBar(),
-      body: Container(
-        padding: const EdgeInsets.all(40.0),
-        child: Column(
-          children: [
-            ElevatedButton(
-                onPressed: () =>
-                    Navigator.pushNamed(context, register),
-                child: const Text("go to register page")),
-            const Text("Login page"),
-          ],
+      backgroundColor: Colors.lightBlue,
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                //Logo
+                const Icon(
+                  Icons.photo_camera_outlined,
+                  color: Colors.white,
+                  size: 150,
+                ),
+                // App Name
+                const Text(
+                  'Art Share',
+                  style: TextStyle(
+                    fontSize: 50,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                // Input
+                const SizedBox(height: 30),
+                usernameField,
+                const SizedBox(height: 10),
+                passwordField,
+                //
+                const SizedBox(height: 10),
+                signInButton,
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text(
+                      'Not a User? ',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 13,
+                      ),
+                    ),
+                    Text(
+                      'Sign up now.',
+                      style: TextStyle(
+                        color: Colors.indigoAccent,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 13,
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
