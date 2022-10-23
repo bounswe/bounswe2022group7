c523ebe0e51d5d7e4c61053701d57ff47a9f535d
AlicanM <alicanmilani@hotmail.com>
2022-10-23 23:06:28 +0300

    controllers and navigators are added to login page

--- android/lib/pages/login.dart
+++ android/lib/pages/login.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../config/app_routes.dart';
+import '../util/snack_bar.dart';
 import '../widgets/form_app_bar.dart';
 
 class Login extends StatefulWidget {
@@ -11,73 +12,130 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
-  Widget usernameField = Padding(
-    padding: const EdgeInsets.symmetric(horizontal: 25.0),
-    child: Container(
-      decoration: BoxDecoration(
-        color: Colors.white,
-        border: Border.all(color: Colors.black),
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: const Padding(
-        padding: EdgeInsets.only(left: 20.0),
-        child: TextField(
-          autofocus: false,
-          decoration: InputDecoration(
-            border: InputBorder.none,
-            hintText: 'Username',
-          ),
-        ),
-      ),
-    ),
-  );
+// Controllers
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
 
-  Widget passwordField = Padding(
-    padding: const EdgeInsets.symmetric(horizontal: 25.0),
-    child: Container(
-      decoration: BoxDecoration(
-        color: Colors.white,
-        border: Border.all(color: Colors.black),
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: const Padding(
-        padding: EdgeInsets.only(left: 20.0),
-        child: TextField(
-          autofocus: false,
-          obscureText: true,
-          decoration: InputDecoration(
-            border: InputBorder.none,
-            hintText: 'Password',
-          ),
-        ),
-      ),
-    ),
-  );
+  void logIn() {
+    final username = _usernameController.text.trim();
+    final password = _passwordController.text;
 
-  Widget signInButton = Padding(
-    padding: const EdgeInsets.symmetric(horizontal: 25.0),
-    child: Container(
-      padding: const EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        color: Colors.indigo,
-        border: Border.all(color: Colors.black),
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: const Center(
-        child: Text(
-          'Log In',
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-            fontSize: 18,
-          ),
-        ),
-      ),
-    ),
-  );
+    // delete every route in navigation stack before navigating to homepage
+    Navigator.pushNamedAndRemoveUntil(
+      context,
+      homepage,
+      (route) => false,
+    );
+    return;
+  }
+
+  void signUp() {
+    Navigator.pushNamed(context, register);
+  }
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
+    Widget usernameField = Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(color: Colors.black),
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: const Padding(
+          padding: EdgeInsets.only(left: 20.0),
+          child: TextField(
+            //controller: _usernameController,
+            autofocus: false,
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              hintText: 'Username',
+            ),
+          ),
+        ),
+      ),
+    );
+
+    Widget passwordField = Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(color: Colors.black),
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: const Padding(
+          padding: EdgeInsets.only(left: 20.0),
+          child: TextField(
+            //controller: _passwordController,
+            autofocus: false,
+            obscureText: true,
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              hintText: 'Password',
+            ),
+          ),
+        ),
+      ),
+    );
+
+    Widget logInButton = Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      child: GestureDetector(
+        onTap: logIn,
+        child: Container(
+          padding: const EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: Colors.indigo,
+            border: Border.all(color: Colors.black),
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: const Center(
+            child: Text(
+              'Log In',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+
+    Widget signUpButton = GestureDetector(
+      onTap: signUp,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          Text(
+            'Not a User? ',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 13,
+            ),
+          ),
+          Text(
+            'Sign up now.',
+            style: TextStyle(
+              color: Colors.deepPurple,
+              fontWeight: FontWeight.bold,
+              fontSize: 13,
+            ),
+          )
+        ],
+      ),
+    );
+
     return Scaffold(
       backgroundColor: Colors.lightBlue,
       body: SafeArea(
@@ -105,30 +163,11 @@ class _LoginState extends State<Login> {
                 usernameField,
                 const SizedBox(height: 10),
                 passwordField,
-                //
+                // Buttons
                 const SizedBox(height: 10),
-                signInButton,
+                logInButton,
                 const SizedBox(height: 10),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    Text(
-                      'Not a User? ',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 13,
-                      ),
-                    ),
-                    Text(
-                      'Sign up now.',
-                      style: TextStyle(
-                        color: Colors.indigoAccent,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 13,
-                      ),
-                    )
-                  ],
-                )
+                signUpButton,
               ],
             ),
           ),
