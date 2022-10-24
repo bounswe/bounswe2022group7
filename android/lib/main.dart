61e03036747138d91939c9892ac3680e01e72a64
musasimsekdotdll <musa.simsek@boun.edu.tr>
2022-10-24 19:26:53 +0300

    profile page needs a user as a parameter

--- android/lib/main.dart
+++ android/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:android/pages/login.dart';
 import 'package:android/pages/profilePage.dart';
 import 'package:android/pages/register.dart';
 import 'package:flutter/material.dart';
+import 'package:android/models/user_model.dart';
 
 
 import '/config/app_routes.dart';
@@ -16,6 +17,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final User tom = User(
+      name: "Tom Bombadil",
+      imageUrl: "https://avatarfiles.alphacoders.com/935/93509.jpg",
+      email: 'bombadil@anduin.me',
+      username: '@tombadil',
+    );
     return MaterialApp(
       title: 'ArtShare',
       theme: ThemeData(
@@ -25,7 +32,7 @@ class MyApp extends StatelessWidget {
       routes: {
         login: (context) => const Login(),
         register: (context) => const Register(),
-        profile: (context) => const ProfilePage(),
+        profile: (context) => ProfilePage(current_user: tom,),
       },
     );
   }
