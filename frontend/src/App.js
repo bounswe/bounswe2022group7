1d1fab941a9e0ce048f91e5630b0dc1167f7ec8a
CahidArda <57228345+CahidArda@users.noreply.github.com>
2022-10-25 10:14:14 +0300

    Update router (FE, #290)
    Replace RouterProvider with BrowserRouter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,41 +5,47 @@ import {
   RouterProvider,
 } from "react-router-dom";
 
+import { Routes, Route } from "react-router-dom";
+
 import ResponsiveAppBar from './ResponsiveAppBar';
 import HomePage from "./pages/HomePage/HomePage"
+import CreateArtItemPage from "./pages/ArtItemPage/CreateArtItemPage"
 import ArtItemPage from "./pages/ArtItemPage/ArtItemPage"
 import SignInPage from './pages/Authentication/SignInPage';
 import SignUpPage from './pages/Authentication/SignUpPage';
 
-import Container from '@mui/material/Container';
+import { AuthProvider } from './auth/useAuth';
+import {ProtectedRoute} from './auth/ProtectedRoute';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomePage />,
-  },
-  {
-    path: "/auth/signup",
-    element: <SignUpPage />
-  },
-  {
-    path: "/auth/signin",
-    element: <SignInPage />
-  },
-  {
-    // dynamic routing: id parameter can be accessed
-    // in ArtItemPage. See ArtItemPage code to learn
-    // how.
-    path: "/art_item/:id",
-    element: <ArtItemPage />
-  }
-]);
 
 function App() {
   return (
     <div>
-      <ResponsiveAppBar />
-      <RouterProvider router={router} />
+      <AuthProvider>
+        <ResponsiveAppBar />
+        <Routes>
+          <Route
+            path="/"
+            element={<HomePage />}
+          />
+          <Route
+            path="/auth/signup"
+            element={<SignUpPage />}
+          />
+          <Route
+            path="/auth/signin"
+            element={<SignInPage />}
+          />
+          <Route
+            path="/art_item/:id"
+            element={<ArtItemPage />}
+          />
+          <Route
+            path="/new/art_item"
+            element={<ProtectedRoute><CreateArtItemPage /></ProtectedRoute>} 
+          />
+        </Routes>
+      </AuthProvider>
     </div>
   );
 }
