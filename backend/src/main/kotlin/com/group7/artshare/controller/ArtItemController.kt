37844bd23471a1e2894507365febc4c3ed449e16
Başak Önder <basakonder@basak-air.local>
2022-10-29 15:19:44 +0300

    single art item endpoint and data prop endpoint

--- backend/src/main/kotlin/com/group7/artshare/controller/ArtItemController.kt
+++ backend/src/main/kotlin/com/group7/artshare/controller/ArtItemController.kt
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @CrossOrigin(origins = ["*"], allowedHeaders = ["*"])
-@RequestMapping("artItem")
+@RequestMapping("art_item")
 class ArtItemController {
 
     @Autowired
