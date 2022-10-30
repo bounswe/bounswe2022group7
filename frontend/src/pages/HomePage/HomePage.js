5ebd0ed4ce67d1d583483da0f3605182d4c2e6bc
guneyizol <guneyizol@gmail.com>
2022-10-30 13:34:55 +0300

    Switch to cra

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -1,51 +1,77 @@
 import React from 'react'
 
 import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
 
 import { Event } from './Event'
 import { ArtItem } from './ArtItem'
 import { ForumOverview } from './ForumOverview'
 
 class HomePage extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            error: null,
+            isLoaded: false,
+            artitems: [],
+            events: []
+        }
+    }
+
+    componentDidMount() {
+        const fetchHeaders = {
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyMSIsImF1dGhvcml0aWVzIjpbImFydGlzdCJdLCJleHAiOjE2OTg2MjQwMDB9.yXtKaBo2Nxt28f2U2wFwlRPG7HDlc91BSeObt-6UzLM'
+        }
+
+        fetch('/homepage/getGenericArtItems', {
+            headers: fetchHeaders,
+            method: 'GET',
+        })
+            .then((response) => response.text())
+            .then((data) => {
+                console.log(data)
+            });
+    }
+
     render() {
-        return (
-            <Grid
-                container
-            >
-                <Grid container item xs={12} md={4} direction='column' wrap='wrap'>
-                    <Grid item>
-                        <Event />
+        const { error, isLoaded, artitems } = this.state
+        if (error) {
+            return <div>Error: {error.message}</div>
+        } else if (!isLoaded) {
+            return <div>Loading...</div>
+        } else
+            return (
+                <Grid
+                    container
+                >
+                    <Grid container item xs={12} md={4} direction='column' wrap='wrap'>
+                        {artitems.map(_ => (
+                            <Grid item>
+                                <ArtItem />
+                            </Grid>
+                        ))}
                     </Grid>
-                    <Grid item>
-                        <ArtItem />
+                    <Grid container item xs={12} md={4} direction='column' wrap='wrap'>
+                        <Grid item>
+                            <ArtItem />
+                        </Grid>
+                        <Grid item>
+                            <Event />
+                        </Grid>
+                        <Grid item>
+                            <Event />
+                        </Grid>
                     </Grid>
-                    <Grid item>
-                        <Event />
-                    </Grid>
-                </Grid>
-                <Grid container item xs={12} md={4} direction='column' wrap='wrap'>
-                    <Grid item>
-                        <ArtItem />
-                    </Grid>
-                    <Grid item>
-                        <Event />
-                    </Grid>
-                    <Grid item>
-                        <Event />
-                    </Grid>
-                </Grid>
-                <Grid container item xs={12} md={4} direction='column' wrap='wrap' sx={{
-                    display: { xs: 'none', md: 'block' }
-                }}>
-                    <Grid item sx={{
-                        height: '100%'
+                    <Grid container item xs={12} md={4} direction='column' wrap='wrap' sx={{
+                        display: { xs: 'none', md: 'block' }
                     }}>
-                        <ForumOverview />
+                        <Grid item sx={{
+                            height: '100%'
+                        }}>
+                            <ForumOverview />
+                        </Grid>
                     </Grid>
                 </Grid>
-            </Grid>
-        )
+            )
     }
 }
 
