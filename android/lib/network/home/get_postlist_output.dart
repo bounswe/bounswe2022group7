06db62d07bdd46e405f09edbbfea2cdd69e0fe38
AlicanM <alicanmilani@hotmail.com>
2022-10-31 18:22:41 +0300

    combining events and art items infos under post list

---
+++ android/lib/network/home/get_postlist_output.dart
@@ -0,0 +1,68 @@
+import 'package:android/models/models.dart';
+
+class GetPostListOutput {
+  final String status;
+  final List<Post>? list;
+
+  GetPostListOutput({required this.status, this.list});
+
+  GetPostListOutput.combine(
+      GetEventListOutput events /*, GetArtItemListOutput artitems*/)
+      : status = "OK",
+        list = List<Post>.empty(growable: true) {
+    for (final event in events.list) {
+      Post post = Post(
+        type: "Event",
+        id: event.id,
+        creator: event.creator,
+        title: event.eventInfo.title,
+        description: event.eventInfo.description,
+        imageUrl: event.eventInfo.posterUrl,
+      );
+      list?.add(post);
+    }
+    /*for (final artitem in artitems.list) {
+      Post post = Post(
+        type: "Art Item",
+        id: artitem.id,
+        creator: artitem.artist,
+        title: artitem.name,
+        description: artitem.description,
+        imageUrl: artitem.imageUrl,
+      );
+      list?.add(post);
+    }*/
+  }
+}
+
+class GetEventListOutput {
+  final String status;
+  final List<Event> list;
+
+  GetEventListOutput({required this.status, required this.list});
+
+  factory GetEventListOutput.fromJson(Iterable json) {
+    List<Event> events =
+        List<Event>.from(json.map((model) => Event.fromJson(model)));
+    return GetEventListOutput(
+      status: "OK",
+      list: events,
+    );
+  }
+}
+
+/*class GetArtItemListOutput {
+  final String status;
+  final List<ArtItem> list;
+
+  GetArtItemListOutput({required this.status, required this.list});
+
+  factory GetArtItemListOutput.fromJson(Iterable json) {
+    List<ArtItem> artitems =
+        List<ArtItem>.from(json.map((model) => ArtItem.fromJson(model)));
+    return GetArtItemListOutput(
+      status: "OK",
+      list: artitems,
+    );
+  }
+}*/
