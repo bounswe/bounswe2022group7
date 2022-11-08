b888de02e05ff6934760a9ec23d841e13174cf5c
CahidArda <57228345+CahidArda@users.noreply.github.com>
2022-11-08 09:38:24 +0300

    Add form layout

---
+++ frontend/src/layouts/FormLayout.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { CssBaseline, Paper } from '@mui/material';
+
+const styles = {
+  main: {
+    width: 'auto',
+    display: 'block', // Fix IE 11 issue.
+    marginLeft: 100,
+    marginRight: 100,
+  },
+  paper: {
+    marginTop: 10,
+    paddingBottom: 20,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    p: 2,
+    width: 500,
+    marginLeft: "auto",
+    marginRight: "auto",
+  },
+};
+
+const FormLayout =({children}) =>{
+  
+  return (
+    <main SX={styles.main}>
+      <CssBaseline />
+      <Paper sx={styles.paper} elevation={5}>
+        {children}
+      </Paper>
+    </main>
+  );
+}
+
+export default FormLayout;
