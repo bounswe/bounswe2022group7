ae7e0e1cecf553fa0b4a7fe4c123fdae1578ec63
CahidArda <57228345+CahidArda@users.noreply.github.com>
2022-11-21 00:48:46 +0300

    Remove margins from content layout (FE)

--- frontend/src/layouts/ContentLayout.js
+++ frontend/src/layouts/ContentLayout.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import { CssBaseline, Paper } from '@mui/material';
+import { Paper } from '@mui/material';
 
 const styles = {
   main: {
     width: 'auto',
     display: 'block', // Fix IE 11 issue.
-    marginLeft: 100,
-    marginRight: 100,
   },
   paper: {
     marginTop: 3,
     paddingBottom: 20,
     flexDirection: 'column',
     alignItems: 'center',
-    p: 2,
     maxWidth: "95vh",
     width: "100%",
     marginLeft: "auto",
