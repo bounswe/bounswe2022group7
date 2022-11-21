c6a223d99cf063a983ed59e12f1eac219d9538b0
Demet Yayla <dmtyayla@gmail.com>
2022-11-22 00:17:10 +0300

    follow user endpoint

--- backend/src/main/kotlin/com/group7/artshare/controller/ProfileController.kt
+++ backend/src/main/kotlin/com/group7/artshare/controller/ProfileController.kt
@@ -1,14 +1,18 @@
 package com.group7.artshare.controller
 
 import com.group7.artshare.entity.RegisteredUser
+import com.group7.artshare.service.JwtService
 import com.group7.artshare.service.ProfileService
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.server.ResponseStatusException
 
 @RestController
 @CrossOrigin(origins = ["*"], allowedHeaders = ["*"])
 @RequestMapping("profile")
 class ProfileController(
-    private val profileService: ProfileService
+    private val profileService: ProfileService,
+    private val jwtService: JwtService
 ) {
 
     @GetMapping("{username}")
@@ -26,5 +30,24 @@ class ProfileController(
         return profileService.getUserByUsernameOrToken(null, authorizationHeader)
     }
 
+    @PostMapping("/follow/{username}")
+    fun followUser(
+        @PathVariable(value = "username") username: String,
+        @RequestHeader(value = "Authorization", required = true) authorizationHeader: String?
+    ): RegisteredUser {
+        try {
+            authorizationHeader?.let {
+                val user =
+                    jwtService.getUserFromAuthorizationHeader(authorizationHeader) ?: throw Exception("Invalid token")
+                return profileService.followUser(username , user)
+            } ?: throw ResponseStatusException(HttpStatus.BAD_REQUEST, "Unregistered user cannot follow other users")
+        } catch (e: Exception) {
+            if (e.message == "Invalid token") {
+                throw ResponseStatusException(HttpStatus.UNAUTHORIZED, e.message)
+            } else {
+                throw ResponseStatusException(HttpStatus.BAD_REQUEST, e.message)
+            }
+        }
+    }
 
 }
--- backend/src/main/kotlin/com/group7/artshare/service/ProfileService.kt
+++ backend/src/main/kotlin/com/group7/artshare/service/ProfileService.kt
@@ -1,12 +1,21 @@
 package com.group7.artshare.service
 
 import com.group7.artshare.entity.RegisteredUser
+import com.group7.artshare.repository.RegisteredUserRepository
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import org.springframework.web.server.ResponseStatusException
+import java.util.*
 
 @Service
-class ProfileService(private val registeredUserService: RegisteredUserService, private val jwtService: JwtService) {
+class ProfileService(
+    private val registeredUserService: RegisteredUserService,
+    private val jwtService: JwtService
+    ) {
+
+    @Autowired
+    lateinit var registeredUserRepository: RegisteredUserRepository
 
     fun getUserByUsernameOrToken(username: String?, authorizationHeader: String?): RegisteredUser {
         try {
@@ -28,4 +37,14 @@ class ProfileService(private val registeredUserService: RegisteredUserService, p
             }
         }
     }
+
+    fun followUser(username: String , user: RegisteredUser) : RegisteredUser {
+        var followedUser : RegisteredUser? = registeredUserRepository.findByAccountInfo_Username(username)
+        if(Objects.nonNull(followedUser)) {
+            user.following.add(followedUser!!)
+            return registeredUserRepository.save(user)
+        }else
+            throw ResponseStatusException(HttpStatus.BAD_REQUEST, "Could not find user with given username")
+    }
+
 }
