afda0341fba9823acae45ccb618fd64f578ab956
Erim Erkin Doğan <erimerkin@gmail.com>
2022-11-21 17:12:47 +0300

    Implemented CustomOutlinedInput for date/time #418

--- frontend/src/components/CustomOutlinedInput.js
+++ frontend/src/components/CustomOutlinedInput.js
@@ -3,23 +3,33 @@ import InputLabel from '@mui/material/InputLabel';
 import InputHelperText from '@mui/material/FormHelperText';
 import PropTypes from 'prop-types';
 
-export default function DateInput(props) {
+export default function CustomOutlinedInput(props) {
+
+    const handleChange = (event) => {
+        props.onChange(event.target.value);
+    };
 
     return (
         <>
-            <InputLabel htmlFor="uncontrolled-native" id="date-label">
+            <InputLabel htmlFor="uncontrolled-native" id="time-label">
                 {props.label}
             </InputLabel>
-            <OutlinedInput fullWidth={props.fullWidth} name={props.name} id={props.id} type="date" value={props.value} onChange={props.onChange} />
+            <OutlinedInput
+                id="uncontrolled-native"
+                fullWidth={props.fullWidth}
+                name={props.name} 
+                type={props.type || "text"}
+                value={props.value} onChange={(event) => handleChange(event)} />
             <InputHelperText error={props.error} helperText={props.helperText} />
         </>
     )
 }
 
-DateInput.propTypes = {
+CustomOutlinedInput.propTypes = {
     label: PropTypes.string.isRequired,
-    value: PropTypes.string.isRequired,
+    value: PropTypes.string,
     onChange: PropTypes.func.isRequired,
+    type: PropTypes.string,
     name: PropTypes.string,
     error: PropTypes.bool,
     helperText: PropTypes.string,
