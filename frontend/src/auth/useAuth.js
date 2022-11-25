99e975d1c123397e5abb4ade6e5d2ec9da7efefc
CahidArda <57228345+CahidArda@users.noreply.github.com>
2022-11-26 00:12:34 +0300

    Add postRequestWithImage method (FE, #401)

--- frontend/src/auth/useAuth.js
+++ frontend/src/auth/useAuth.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { createContext, useContext, useMemo } from "react";
 import { useNavigate } from "react-router-dom";
 import { useLocalStorage } from "./useLocalStorage";
@@ -21,13 +21,59 @@ export const AuthProvider = ({ children }) => {
     window.location.href = '/'
   };
 
+  const postRequestWithImage = (
+    url, // address we are making a request to
+    base64String, // base64String is the string of the image
+    formatFunction, // a function that receives id of the image and constructs the request body
+    navigateToFunction // a function that constructs a path to navigate to from the response
+    ) => {
+      fetch("/api/image", {
+        method: "POST",
+        body: JSON.stringify({base64String: base64String}),
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer " + token
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          const posterId = data.id
+  
+          fetch(url, {
+            method: "POST",
+            body: JSON.stringify(formatFunction(posterId)),
+            headers: {
+              "Content-Type": "application/json",
+              "Authorization": "Bearer " + token
+            },
+          })
+            .then((response) => response.json())
+            .then((data) => navigate(navigateToFunction(data)))
+        }
+      )
+  }
+
+  const [userData, setUserData] = useState(null)
+  useEffect(() => {
+    fetch('/api/profile', {
+      method: "GET",
+      headers: {
+        "Authorization": "Bearer " + token
+      }
+    })
+      .then(response => response.json())
+      .then(data => setUserData(data))
+  }, [token])
+
   const value = useMemo(
     () => ({
       token,
       saveToken,
-      clearToken
+      clearToken,
+      postRequestWithImage,
+      userData
     }),
-    [token]
+    [token, userData]
   );
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
