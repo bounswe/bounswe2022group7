7b9ea12700a5254c9b5ff0869bef908a1af56953
CahidArda <57228345+CahidArda@users.noreply.github.com>
2022-11-26 19:17:03 +0300

    Add loading GIF (FE, #459 )

--- frontend/src/components/ImageComponent.js
+++ frontend/src/components/ImageComponent.js
@@ -14,7 +14,16 @@ function ImageComponent({imageId}) {
 
   return (
     <>
-      {state.base64String && <img src={state.base64String} alt="image" style={{width: '100%'}}/>}
+      {
+      state.base64String
+      ?
+      <img src={state.base64String} alt="image" style={{width: '100%'}}/>
+      :
+      <div style={{textAlign: "center"}}>
+        <img src="https://www.ign.gob.ar/geodesiaapp/ntrip-registro/img/loader.gif" alt="loading" style={{width: '25%'}}/>
+      </div>
+
+      }
     </>
   )
 }
