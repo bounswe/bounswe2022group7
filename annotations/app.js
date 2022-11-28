450c3d109a83bc94675134ec76d8ccc70656d7d6
guneyizol <guneyizol@gmail.com>
2022-11-28 14:46:04 +0300

    parse application/ld+json request body

--- annotations/app.js
+++ annotations/app.js
@@ -2,6 +2,7 @@ import mongo from 'koa-mongo'
 import Koa from 'koa'
 import Router from '@koa/router'
 import { koaBody } from 'koa-body'
+import bodyParser from 'koa-bodyparser'
 
 const app = new Koa()
 const router = new Router()
@@ -13,14 +14,18 @@ app.use(mongo({
     db: 'admin',
     acquireTimeoutMillis: 10000
 }))
-
+app.use(bodyParser({
+    extendTypes: {
+        json: ['application/ld+json']
+    }
+}))
 router.post('/annotations', koaBody(), async ctx => {
     const result = await ctx.db.collection('annotations').insertOne(ctx.request.body)
+
     ctx.body = { id: ctx.request.body.id }
+    
     ctx.set('Content-Type', 'application/ld+json; charset=utf-8; profile=http://www.w3.org/ns/anno.jsonld')
     ctx.set('ETag', `W/${result.ops[0]._id}`)
-
-    
 })
     .get('/annotations', async ctx => {
         ctx.body = await ctx.db.collection('annotations').find().toArray()
