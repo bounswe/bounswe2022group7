913dd0348180547c94d3d3c2656df5e889332d37
atillaturkmen <turkmenatilla522@gmail.com>
2022-12-02 22:05:06 +0300

    fix art item models for homepage (#484)

--- android/lib/models/art_item/art_item_model.dart
+++ android/lib/models/art_item/art_item_model.dart
@@ -5,10 +5,8 @@ class ArtItem extends Post {
   final DateTime creationDate;
   final User? owner;
   final bool onAuction;
-  final String? auction;
   final double? lastPrice;
   final List<String> commentList;
-  final List<User> bookmarkedBy;
 
   ArtItem({
     required int id,
@@ -17,10 +15,8 @@ class ArtItem extends Post {
     required this.creationDate,
     this.owner,
     required this.onAuction,
-    this.auction,
     this.lastPrice,
     required this.commentList,
-    required this.bookmarkedBy,
   }) : super(
           type: "Art Item",
           id: id,
@@ -29,26 +25,23 @@ class ArtItem extends Post {
         );
 
   factory ArtItem.fromJson(Map<String, dynamic> json) {
+    print("hey");
     ArtItem ai = ArtItem(
       id: json['id'] ?? 8,
       artItemInfo: ArtItemInfo.fromJson(json),
-      creatorAccountInfo: json['creatorAccountInfo'],
+      creatorAccountInfo: AccountInfo.fromJson(json['creatorAccountInfo']),
       creationDate: DateTime.parse(json['creationDate']),
       // why does this use accountInfo?
       // owner: User.fromJson(json['owner']["accountInfo"]),
 
       // Auction model has not been implemented yet
       onAuction: false,
-      auction: null,
 
       lastPrice: json['lastPrice'],
 
       // Comment model has not been implemented yet just store as strings
       commentList:
           List<String>.from(json['commentList'].map((x) => x.toString())),
-
-      bookmarkedBy:
-          List<User>.from(json['bookMarkedByIds'].map((x) => User.fromJson(x))),
     );
 
     return ai;
