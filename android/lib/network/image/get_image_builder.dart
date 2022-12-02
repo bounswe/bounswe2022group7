e9844b607b3efd261b0ae98b7b117ca853df69d1
bnbcfyh <56476673+bnbcfyh@users.noreply.github.com>
2022-12-03 00:09:36 +0300

    Add image builder functions with extra utility

--- android/lib/network/image/get_image_builder.dart
+++ android/lib/network/image/get_image_builder.dart
@@ -48,3 +48,96 @@ Widget imageBuilder(int? imageId) {
     },
   );
 }
+
+Widget imageBuilderWithSize(int? imageId, double width, double height) {
+  if (imageId == null) return Container();
+  return FutureBuilder(
+    future: getImageNetwork(imageId),
+    builder: (context, snapshot) {
+      switch (snapshot.connectionState) {
+        case ConnectionState.none:
+        case ConnectionState.waiting:
+          return const CircularProgressIndicator();
+        default:
+          if (snapshot.hasError) {
+            return Scaffold(
+              appBar: AppBar(
+                title: const Text("Snapshot Error!"),
+              ),
+              body: Center(
+                child: Text("Error: ${snapshot.error}"),
+              ),
+            );
+          }
+
+          if (snapshot.data != null) {
+            GetImageOutput responseData = snapshot.data!;
+            if (responseData.status != "OK") {
+              return Container();
+            }
+            ImageModel currentImage = responseData.image!;
+            return currentImage.base64String != null
+                ? Image.memory(base64Decode(
+                    currentImage.base64String.contains("data:image/png;base64,")
+                        ? currentImage.base64String
+                            .split("data:image/png;base64,")
+                            .elementAt(1)
+                        : currentImage.base64String),
+                    width: width,
+                    height: height)
+                : Container();
+          } else {
+            // snapshot.data == null
+            return Container();
+          }
+      }
+    },
+  );
+}
+
+Widget circleAvatarBuilder(int? imageId, double radius) {
+  if (imageId == null) return Container();
+  return FutureBuilder(
+    future: getImageNetwork(imageId),
+    builder: (context, snapshot) {
+      switch (snapshot.connectionState) {
+        case ConnectionState.none:
+        case ConnectionState.waiting:
+          return const CircularProgressIndicator();
+        default:
+          if (snapshot.hasError) {
+            return Scaffold(
+              appBar: AppBar(
+                title: const Text("Snapshot Error!"),
+              ),
+              body: Center(
+                child: Text("Error: ${snapshot.error}"),
+              ),
+            );
+          }
+
+          if (snapshot.data != null) {
+            GetImageOutput responseData = snapshot.data!;
+            if (responseData.status != "OK") {
+              return Icon(Icons.person, size: 2 * radius);
+            }
+            ImageModel currentImage = responseData.image!;
+            return currentImage.base64String != null
+                ? CircleAvatar(
+                    radius: radius,
+                    backgroundImage: MemoryImage(base64Decode(
+                        currentImage.base64String.contains(
+                                "data:image/png;base64,")
+                            ? currentImage.base64String
+                                .split("data:image/png;base64,")
+                                .elementAt(1)
+                            : currentImage.base64String)))
+                :  Icon(Icons.person, size: 2 * radius);
+          } else {
+            // snapshot.data == null
+            return Icon(Icons.person, size: 2 * radius);
+          }
+      }
+    },
+  );
+}
