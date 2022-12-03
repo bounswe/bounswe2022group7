2d2064be672c4f5abcb293f1354523fe8b09ab65
Erim Erkin Doğan <erimerkin@gmail.com>
2022-12-03 20:43:27 +0300

    Implement a customizable menu component (#487)

---
+++ frontend/src/components/CustomizableDropdownMenu.js
@@ -0,0 +1,97 @@
+import * as React from 'react';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+
+export default function CustomizableDropdownMenu({tooltip, menuIcon, menuContent}) {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  return (
+    <React.Fragment>
+      <Tooltip title={tooltip}>
+        <IconButton
+          onClick={handleClick}
+          size="small"
+          sx={{ ml: 2 }}
+          aria-controls={open ? 'account-menu' : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? 'true' : undefined}
+        >
+          {menuIcon}
+        </IconButton>
+      </Tooltip>
+      <Menu
+        anchorEl={anchorEl}
+        id="account-menu"
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        PaperProps={{
+          elevation: 0,
+          sx: {
+            overflow: 'visible',
+            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+            mt: 1.5,
+            '& .MuiAvatar-root': {
+              width: 32,
+              height: 32,
+              ml: -0.5,
+              mr: 1,
+            },
+            '&:before': {
+              content: '""',
+              display: 'block',
+              position: 'absolute',
+              top: 0,
+              right: 14,
+              width: 10,
+              height: 10,
+              bgcolor: 'background.paper',
+              transform: 'translateY(-50%) rotate(45deg)',
+              zIndex: 0,
+            },
+          },
+        }}
+        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+      >
+        {menuContent.map((item, index) => (
+          <MenuItem key={index} onClick={item.action}>
+            <ListItemIcon>
+              {React.cloneElement(item.icon, { fontSize: 'small' })}
+            </ListItemIcon>
+            {item.label}
+          </MenuItem>
+        ))}
+      </Menu>
+    </React.Fragment>
+  );
+}
+
+CustomizableDropdownMenu.defaultProps = {
+  tooltip: "Menu",
+  menuIcon: <MoreVertIcon />,
+  menuContent: [
+    {
+      label: "Profile",
+      icon: <AccountCircleIcon />,
+      action: () => console.log("Profile")
+    },
+    {
+      label: "Logout",
+      icon: <ExitToAppIcon />,
+      action: () => console.log("Logout")
+    }
+  ]
+}
