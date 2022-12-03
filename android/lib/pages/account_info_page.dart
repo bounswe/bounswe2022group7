61a8b851a37ed13f4ad41a545dd4c5f7cad2677d
bnbcfyh <56476673+bnbcfyh@users.noreply.github.com>
2022-12-03 10:29:22 +0300

    Improve Account Info UI

--- android/lib/pages/account_info_page.dart
+++ android/lib/pages/account_info_page.dart
@@ -46,120 +46,218 @@ class _AccountInfoPageState extends State<AccountInfoPage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Row(
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Username: ",
-                      style: const TextStyle(
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.bold,
+            Container(
+              padding: const EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.blue[100]!),
+                borderRadius: BorderRadius.circular(10.0),
+                color: Colors.blue[100],
+              ),
+              child: Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Username: ",
+                        style: const TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
-                    Text(
-                      "${widget.username}",
-                      style: const TextStyle(
-                        fontSize: 16.0,
+                      Text(
+                        "${widget.username}",
+                        style: const TextStyle(
+                          fontSize: 16.0,
+                        ),
                       ),
-                    ),
-                    const SizedBox(height: 15),
-                    const Text(
-                      "Email Address: ",
-                      style: TextStyle(
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.bold,
+                      const SizedBox(height: 15),
+                      const Text(
+                        "Email Address: ",
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
-                    Text(
-                      "${widget.email}",
-                      style: const TextStyle(
-                        fontSize: 16.0,
+                      Text(
+                        "${widget.email}",
+                        style: const TextStyle(
+                          fontSize: 16.0,
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-                const Spacer(),
-                if (widget.profilePictureId != null)
-                  imageBuilderWithSize(widget.profilePictureId!, 150, 150)
-                else
-                  const Icon(Icons.account_circle, size: 150),
-              ],
+                    ],
+                  ),
+                  const Spacer(),
+                  if (widget.profilePictureId != null)
+                    imageBuilderWithSize(widget.profilePictureId!, 150, 150)
+                  else
+                    const Icon(Icons.account_circle, size: 150),
+                ],
+              ),
             ),
             const SizedBox(height: 30.0),
-            Row(
-              children: [
-                const Text(
-                  "Name: ",
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.bold,
+            Container(
+              padding: const EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.blue[700]!),
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Column(
+                children: [
+                  const Center(
+                    child: Text("Personal Information",
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold,
+                        )),
                   ),
-                ),
-                Text(
-                  "${widget.name}",
-                  style: const TextStyle(
-                    fontSize: 16.0,
+                  const SizedBox(height: 30.0),
+                  Row(
+                    children: [
+                      const Text(
+                        "Name: ",
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      if (widget.name != null)
+                        Text(
+                          "${widget.name}",
+                          style: const TextStyle(
+                            fontSize: 16.0,
+                          ),
+                        )
+                      else
+                        const Text(
+                          "N/A",
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            color: Colors.grey,
+                          ),
+                        ),
+                    ],
                   ),
-                ),
-              ],
+                  const SizedBox(height: 15.0),
+                  Row(
+                    children: [
+                      const Text(
+                        "Surname: ",
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      if (widget.surname != null)
+                        Text(
+                          "${widget.surname}",
+                          style: const TextStyle(
+                            fontSize: 16.0,
+                          ),
+                        )
+                      else
+                        const Text(
+                          "N/A",
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            color: Colors.grey,
+                          ),
+                        ),
+                    ],
+                  ),
+                  const SizedBox(height: 15.0),
+                  Row(
+                    children: [
+                      const Text(
+                        "Country: ",
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      if (widget.country != null)
+                        Text(
+                          "${widget.country}",
+                          style: const TextStyle(
+                            fontSize: 16.0,
+                          ),
+                        )
+                      else
+                        const Text(
+                          "N/A",
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            color: Colors.grey,
+                          ),
+                        ),
+                    ],
+                  ),
+                  const SizedBox(height: 15.0),
+                  Row(
+                    children: [
+                      const Text(
+                        "Date of Birth: ",
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      if (widget.dateOfBirth != null)
+                        Text(
+                          "${widget.dateOfBirth}",
+                          style: const TextStyle(
+                            fontSize: 16.0,
+                          ),
+                        )
+                      else
+                        const Text(
+                          "N/A",
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            color: Colors.grey,
+                          ),
+                        ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-            const SizedBox(height: 15.0),
-            Row(
-              children: [
-                const Text(
-                  "Surname: ",
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.bold,
+            // show a warning box for user to fill the missing info:
+            if (widget.name == null ||
+                widget.surname == null ||
+                widget.country == null ||
+                widget.dateOfBirth == null)
+              Column(
+                children: [
+                  const SizedBox(height: 30.0),
+                  Container(
+                    padding: const EdgeInsets.all(10.0),
+                    decoration: BoxDecoration(
+                      color: Colors.red[100],
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: const Text(
+                      "Please complete your personal information to get the best experience!",
+                      style: TextStyle(
+                        fontSize: 16.0,
+                      ),
+                    ),
                   ),
+                ],
+              ),
+            const SizedBox(height: 30.0),
+            ElevatedButton(
+              onPressed: () {
+                //Navigator.pushNamed(context, editAccountInfo);
+              },
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.white,
+                backgroundColor: Colors.blue[300],
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
                 ),
-                Text(
-                  "${widget.surname}",
-                  style: const TextStyle(
-                    fontSize: 16.0,
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 15.0),
-            Row(
-              children: [
-                const Text(
-                  "Country: ",
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Text(
-                  "${widget.country}",
-                  style: const TextStyle(
-                    fontSize: 16.0,
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 15.0),
-            Row(
-              children: [
-                const Text(
-                  "Date of Birth: ",
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Text(
-                  "${widget.dateOfBirth}",
-                  style: const TextStyle(
-                    fontSize: 16.0,
-                  ),
-                ),
-
-
-              ],
+              ),
+              child: const Text("Edit Account Info"),
             ),
           ],
         ),
