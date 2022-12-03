c438e8385d72fd12e74de85325430d703ff65de4
guneyizol <guneyizol@gmail.com>
2022-12-04 02:19:03 +0300

    annotations crud

--- annotations/app.js
+++ annotations/app.js
@@ -3,9 +3,11 @@ import Koa from 'koa'
 import Router from '@koa/router'
 import { koaBody } from 'koa-body'
 import bodyParser from 'koa-bodyparser'
+import { ObjectId } from 'mongodb'
 
 const app = new Koa()
 const router = new Router()
+
 
 app.use(mongo({
     host: 'mongo',
@@ -14,21 +16,45 @@ app.use(mongo({
     db: 'admin',
     acquireTimeoutMillis: 10000
 }))
+
 app.use(bodyParser({
     extendTypes: {
         json: ['application/ld+json']
     }
 }))
+
 router.post('/annotations', koaBody(), async ctx => {
     const result = await ctx.db.collection('annotations').insertOne(ctx.request.body)
 
     ctx.body = { id: ctx.request.body.id }
-    
+
     ctx.set('Content-Type', 'application/ld+json; charset=utf-8; profile=http://www.w3.org/ns/anno.jsonld')
-    ctx.set('ETag', `W/${result.ops[0]._id}`)
+    ctx.set('ETag', `W/${result.insertedId}`)
 })
     .get('/annotations', async ctx => {
         ctx.body = await ctx.db.collection('annotations').find().toArray()
+    })
+    .put('/annotations', koaBody(), async ctx => {
+        const result = await ctx.db.collection('annotations').updateOne({
+            _id: ObjectId(ctx.request.body._id)
+        }, {
+            $set: {
+                body: ctx.request.body.body
+            }
+        })
+        ctx.body = result.modifiedCount
+    })
+    .del('/annotations', async ctx => {
+        const result = await ctx.db.collection('annotations').deleteOne({
+            _id: ObjectId(ctx.request.body._id)
+        })
+
+        ctx.body = result.deletedCount
+    })
+    .get('/annotations/:imageId', async ctx => {
+        ctx.body = await ctx.db.collection('annotations').find({
+            $where: `this.id.startsWith(${ctx.params.imageId})`
+        }).toArray()
     })
 
 app.use(router.routes())
