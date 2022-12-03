b3298739a6394e3c48cea10dd42240b0a84e2f46
Erim Erkin Doğan <erimerkin@gmail.com>
2022-12-03 22:10:40 +0300

    Update genericcardlayout for adjustable margins

--- frontend/src/layouts/GenericCardLayout.js
+++ frontend/src/layouts/GenericCardLayout.js
@@ -11,7 +11,7 @@ const styles = {
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
-        marginTop: 4,
+        // marginTop: 4,
         marginBottom: 4,
         paddingTop: 4,
         paddingBottom: 2,
@@ -22,10 +22,12 @@ const styles = {
 };
 
 export default function GenericCardLayout(props) {
+
+
     return (
         <main sx={styles.main}>
             <CssBaseline />
-            <Paper sx={{ ...styles.paper, maxWidth: props.maxWidth || "750" }} variant="outlined">
+            <Paper sx={{ ...styles.paper, maxWidth: props.maxWidth, marginTop: props.customTopMargin }} variant="outlined">
                 <Container>
                     {props.children}
                 </Container>
@@ -34,3 +36,7 @@ export default function GenericCardLayout(props) {
     );
 }
 
+GenericCardLayout.defaultProps = {
+    maxWidth: 750,
+    customTopMargin: 4,
+};
