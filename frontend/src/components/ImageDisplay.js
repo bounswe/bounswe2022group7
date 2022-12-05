b8ebbfd8feae6e760a49f0fb9cc89d009bc3f125
CahidArda <57228345+CahidArda@users.noreply.github.com>
2022-12-05 13:02:09 +0300

    Remove image maxHeight to fix overflow (FE, fix)

--- frontend/src/components/ImageDisplay.js
+++ frontend/src/components/ImageDisplay.js
@@ -15,8 +15,17 @@ import ImageComponent from "./ImageComponent"
 function ImageDisplay({imageId}) {
   return (
     <Paper elevation={0} style={{display: 'flex', width: "100%"}}>
-      <div style={{maxHeight: 400, textAlign: "center", backgroundColor: '#fafafa', width: "100%"}}>
-        <ImageComponent imageId={imageId} imageStyle={{maxWidth: "80%", display: "block", margin: "auto", height: '100%'}}/>
+      <div style={{
+        textAlign: "center",
+        backgroundColor: '#fafafa',
+        width: "100%"
+      }}>
+        <ImageComponent
+          imageId={imageId}
+          imageStyle={{
+            maxWidth: "80%",
+            margin: "auto"}}
+        />
       </div>  
     </Paper>
   )
