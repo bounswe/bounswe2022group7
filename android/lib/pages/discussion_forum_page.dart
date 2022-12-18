26ab9c7a85b8148810d13a5d0818fa858a6a7779
AlicanM <alicanmilani@hotmail.com>
2022-12-18 18:49:43 +0300

    Update discussions page buttons on load

--- android/lib/pages/discussion_forum_page.dart
+++ android/lib/pages/discussion_forum_page.dart
@@ -1,5 +1,5 @@
-import 'package:android/network/discussion/get_discussionlist_output.dart';
-import 'package:android/network/discussion/get_discussionlist_service.dart';
+import 'package:android/network/discussion/post_discussion_vote_input.dart';
+import 'package:android/network/discussion/post_discussion_vote_service.dart';
 import 'package:android/network/image/get_image_builder.dart';
 import 'package:android/pages/discussion_page.dart';
 import 'package:android/pages/home_page.dart';
@@ -10,112 +10,12 @@ import 'package:provider/provider.dart';
 
 import '../providers/user_provider.dart';
 
-Widget buildDiscussion(BuildContext context, Discussion discussion) {
-  return Container(
-      margin: const EdgeInsets.symmetric(vertical: 5.0),
-      padding: const EdgeInsets.symmetric(vertical: 10.0),
-      color: Colors.blue[100],
-      child: InkWell(
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => DiscussionPage(id: discussion.id),
-              ),
-            );
-          },
-          child: Column(children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 12.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              children: [
-                                circleAvatarBuilder(
-                                    discussion
-                                        .creatorAccountInfo.profile_picture_id,
-                                    20.0),
-                                const SizedBox(width: 10.0),
-                                Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        discussion.title,
-                                        style: const TextStyle(
-                                          fontSize: 16.0,
-                                          fontWeight: FontWeight.w600,
-                                        ),
-                                        overflow: TextOverflow.ellipsis,
-                                      ),
-                                      const SizedBox(height: 4.0),
-                                      Row(
-                                        children: [
-                                          Icon(Icons.supervisor_account,
-                                              size: 12.0,
-                                              color: Colors.grey[600]),
-                                          const SizedBox(width: 5.0),
-                                          Text(discussion
-                                              .creatorAccountInfo.username),
-                                        ],
-                                      )
-                                    ]),
-                                const Spacer(),
-                                IconButton(
-                                    onPressed: () {},
-                                    icon: const Icon(Icons.arrow_downward,
-                                        color: Colors.deepOrange, size: 30.0)),
-                                IconButton(
-                                    onPressed: () {},
-                                    icon: const Icon(Icons.arrow_upward,
-                                        color: Colors.green, size: 30.0)),
-                              ],
-                            ),
-                            const SizedBox(height: 10.0),
-                            Row(
-                              children: [
-                                Icon(
-                                  Icons.calendar_month,
-                                  color: Colors.grey[600],
-                                  size: 12.0,
-                                ),
-                                const SizedBox(width: 5.0),
-                                Text(
-                                  discussion.creationDate
-                                      .toString()
-                                      .substring(0, 16),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 10.0),
-                  Text(
-                    discussion.textBody,
-                    style: const TextStyle(
-                      fontSize: 14.0,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                  const SizedBox(height: 4.0),
-                ],
-              ),
-            ),
-          ])));
-}
+List<Discussion> discussions = [];
 
 class DiscussionForumPage extends StatefulWidget {
-  const DiscussionForumPage({Key? key}) : super(key: key);
+  DiscussionForumPage({Key? key, required discussionList}) : super(key: key) {
+    discussions = discussionList;
+  }
 
   @override
   State<DiscussionForumPage> createState() => _DiscussionForumPage();
@@ -130,22 +30,175 @@ class _DiscussionForumPage extends State<DiscussionForumPage> {
     });
   }
 
-  Scaffold discussionForumPageScaffold(
-      CurrentUser? user, List<Discussion> discussionList) {
+  Widget buildDiscussion(BuildContext context, CurrentUser? user, int index) {
+    return Container(
+        margin: const EdgeInsets.symmetric(vertical: 5.0),
+        padding: const EdgeInsets.symmetric(vertical: 10.0),
+        color: Colors.blue[100],
+        child: InkWell(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) =>
+                      DiscussionPage(id: discussions[index].id),
+                ),
+              );
+            },
+            child: Column(children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: [
+                                  circleAvatarBuilder(
+                                      discussions[index]
+                                          .creatorAccountInfo
+                                          .profile_picture_id,
+                                      20.0),
+                                  const SizedBox(width: 10.0),
+                                  Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          discussions[index].title,
+                                          style: const TextStyle(
+                                            fontSize: 16.0,
+                                            fontWeight: FontWeight.w600,
+                                          ),
+                                          overflow: TextOverflow.ellipsis,
+                                        ),
+                                        const SizedBox(height: 4.0),
+                                        Row(
+                                          children: [
+                                            Icon(Icons.supervisor_account,
+                                                size: 12.0,
+                                                color: Colors.grey[600]),
+                                            const SizedBox(width: 5.0),
+                                            Text(discussions[index]
+                                                .creatorAccountInfo
+                                                .username),
+                                          ],
+                                        )
+                                      ]),
+                                  const Spacer(),
+                                  IconButton(
+                                      onPressed: () async {
+                                        if (user != null) {
+                                          final output =
+                                              await postDiscussionVoteNetwork(
+                                                  PostDiscussionVoteInput(
+                                                      id: discussions[index].id,
+                                                      vote: -1));
+                                          setState(() {
+                                            if (output.discussion != null) {
+                                              discussions[index] =
+                                                  output.discussion!;
+                                              discussions[index]
+                                                  .updateVote(user.username);
+                                            } else {
+                                              discussions[index].voteStatus =
+                                                  -1;
+                                            }
+                                          });
+                                        }
+                                      },
+                                      icon: Icon(Icons.arrow_downward,
+                                          color:
+                                              discussions[index].voteStatus ==
+                                                      -1
+                                                  ? Colors.deepOrange
+                                                  : Colors.grey,
+                                          size: 30.0)),
+                                  IconButton(
+                                      onPressed: () async {
+                                        if (user != null) {
+                                          final output =
+                                              await postDiscussionVoteNetwork(
+                                                  PostDiscussionVoteInput(
+                                                      id: discussions[index].id,
+                                                      vote: 1));
+                                          setState(() {
+                                            if (output.discussion != null) {
+                                              discussions[index] =
+                                                  output.discussion!;
+                                              discussions[index]
+                                                  .updateVote(user.username);
+                                            } else {
+                                              discussions[index].voteStatus = 1;
+                                            }
+                                          });
+                                        }
+                                      },
+                                      icon: Icon(Icons.arrow_upward,
+                                          color:
+                                              discussions[index].voteStatus == 1
+                                                  ? Colors.green
+                                                  : Colors.grey,
+                                          size: 30.0)),
+                                ],
+                              ),
+                              const SizedBox(height: 10.0),
+                              Row(
+                                children: [
+                                  Icon(
+                                    Icons.calendar_month,
+                                    color: Colors.grey[600],
+                                    size: 12.0,
+                                  ),
+                                  const SizedBox(width: 5.0),
+                                  Text(
+                                    discussions[index]
+                                        .creationDate
+                                        .toString()
+                                        .substring(0, 16),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 10.0),
+                    Text(
+                      discussions[index].textBody,
+                      style: const TextStyle(
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    const SizedBox(height: 4.0),
+                  ],
+                ),
+              ),
+            ])));
+  }
+
+  Scaffold discussionForumPageScaffold(CurrentUser? user) {
     return Scaffold(
       body: CustomScrollView(slivers: [
         mainAppBar(context, user),
         SliverList(
           delegate: SliverChildBuilderDelegate(
             (context, index) {
-              return buildDiscussion(context, discussionList[index]);
+              return buildDiscussion(context, user, index);
             },
-            childCount: discussionList.length,
+            childCount: discussions.length,
           ),
         ),
       ]),
       drawer: mainDrawer(context, user, logout),
-      bottomNavigationBar: mainBottomBar(context, 2),
+      bottomNavigationBar: mainBottomBar(context, user, 2),
     );
   }
 
@@ -163,39 +216,14 @@ class _DiscussionForumPage extends State<DiscussionForumPage> {
   @override
   Widget build(BuildContext context) {
     CurrentUser? user = Provider.of<UserProvider>(context).user;
-
-    return FutureBuilder(
-      future: getDiscussionPosts(),
-      builder: (context, snapshot) {
-        switch (snapshot.connectionState) {
-          case ConnectionState.none:
-          case ConnectionState.waiting:
-            return const CircularProgressIndicator();
-          default:
-            if (snapshot.hasError) {
-              return Scaffold(
-                appBar: AppBar(
-                  title: const Text("Snapshot Error!"),
-                ),
-                body: Center(
-                  child: Text("Error: ${snapshot.error}"),
-                ),
-              );
-            }
-
-            if (snapshot.data != null) {
-              GetDiscussionListOutput responseData = snapshot.data!;
-              if (responseData.status != "OK") {
-                return erroneousDiscussionForumPage();
-              }
-              List<Discussion> discussionList = responseData.list!;
-              return discussionForumPageScaffold(user, discussionList);
-            } else {
-              // snapshot.data == null
-              return erroneousDiscussionForumPage();
-            }
-        }
-      },
-    );
+    if (discussions.isEmpty) {
+      return erroneousDiscussionForumPage();
+    }
+    /*if (user != null) {
+      for (var disc in discussions) {
+        disc.updateVote(user!.username);
+      }
+    }*/
+    return discussionForumPageScaffold(user);
   }
 }
