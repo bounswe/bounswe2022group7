02499108ca2b26e3a827895857cb7dba05900be9
Demet Yayla <dmtyayla@gmail.com>
2022-12-21 22:42:45 +0300

    reverted-auto-index-left-sqls

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -1,11 +1,11 @@
-ALTER TABLE ${MYSQL_DATABASE}.art_item_info
+ALTER TABLE artshare.art_item_info
     ADD FULLTEXT(name, description, category, labels);
 
-ALTER TABLE ${MYSQL_DATABASE}.event_info
+ALTER TABLE artshare.event_info
     ADD INDEX(title, description, category, labels);
 
-ALTER TABLE ${MYSQL_DATABASE}.discussion_ost
+ALTER TABLE artshare.discussion_ost
     ADD FULLTEXT(title, text_body);
 
-ALTER TABLE ${MYSQL_DATABASE}.account_info
+ALTER TABLE artshare.account_info
     ADD FULLTEXT(name, surname, username, country);
