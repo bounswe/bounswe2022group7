65f516f6d715c717d5ef835292d89f092e152b2d
guneyizol <guneyizol@gmail.com>
2022-12-25 14:09:08 +0300

    use promise api

--- annotations/app.js
+++ annotations/app.js
@@ -12,28 +12,32 @@ dotenv.config()
 const app = new Koa()
 const router = new Router()
 
-var uri = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/`;
-let db_name = process.env.MONGO_DB;
-
+let uri = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/`
 if (process.env.NODE_ENV === "production") {
-    uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_HOST}/?retryWrites=true&w=majority`;
-} 
-
-const client = new MongoClient(uri, {maxPoolSize:10, minPoolSize: 2, useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-
-async function connectDB() {
-    try {
-        // Connect the client to the server (optional starting in v4.7)
-        await client.connect();
-        // Establish and verify connection
-        await client.db(db_name).command({ ping: 1 });
-    } catch (e) {
-        console.error(e);
-    }
+    uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_HOST}/?retryWrites=true&w=majority`
 }
 
-connectDB();
+const client = new MongoClient(
+    uri,
+    {
+        maxPoolSize: 10,
+        minPoolSize: 2,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverApi: ServerApiVersion.v1
+    }
+)
+
+
+// Connect the client to the server (optional starting in v4.7)
+await client.connect()
+    .then(client => {
+        // Establish and verify connection
+        client.db(db_name).command({ ping: 1 })
+    })
+    .catch(err => console.error(err.message))
+
+let annotations = client.db(process.env.MONGO_DB).collection('annotations')
 
 app.use(bodyParser({
     extendTypes: {
@@ -41,77 +45,49 @@ app.use(bodyParser({
     }
 }))
 
-
-async function findOperations(query = null) {
-    try {
-        const database = client.db(db_name);
-        const annotations = database.collection("annotations");
-
-        return await annotations.find(query).toArray();
-
-    } catch (e) {
-        console.error(e);
-    }
-}
+const findOperations = async (query = null) =>
+    annotations.find(query).toArray()
 
 router.post('/annotations', koaBody(), async ctx => {
-    try {
-        const database = client.db(db_name);
-        const annotations = database.collection("annotations");
-        const result = await annotations.insertOne(ctx.request.body);
-
-        ctx.body = { id: ctx.request.body.id }
-
-        ctx.set('Content-Type', 'application/ld+json; charset=utf-8; profile=http://www.w3.org/ns/anno.jsonld')
-        ctx.set('ETag', `W/${result.insertedId}`)
-
-    } catch (e) {
-        console.error(e);
-    }
+    annotations.insertOne(ctx.request.body)
+        .then(result => {
+            ctx.body = { id: ctx.request.body.id }
+            ctx.set(
+                'Content-Type',
+                'application/ld+json; charset=utf-8; profile=http://www.w3.org/ns/anno.jsonld'
+            )
+            ctx.set('ETag', `W/${result.insertedId}`)
+        })
+        .catch(err => console.error(err.message))
 })
     .get('/annotations', async ctx => {
-        ctx.body = await findOperations();
+        ctx.body = await findOperations()
     })
     .put('/annotations', koaBody(), async ctx => {
-
-        try {
-            const database = client.db(db_name);
-            const annotations = database.collection("annotations");
-            const result = await annotations.updateOne(
-                { _id: ObjectId(ctx.request.body._id)}, {
-                    $set: {
-                        body: ctx.request.body.body
-                    }
-                });
-    
-            ctx.body = result.modifiedCount
-    
-        } catch (e) {
-            console.error(e);
-        }
+        annotations.updateOne(
+            {
+                _id: ObjectId(ctx.request.body._id)
+            },
+            {
+                $set: {
+                    body: ctx.request.body.body
+                }
+            }).then(result => {
+                ctx.body = result.modifiedCount
+            })
+            .catch(err => console.error(err.message))
     })
     .del('/annotations', async ctx => {
-
-        try {
-            const database = client.db(db_name);
-            const annotations = database.collection("annotations");
-            const result = await annotations.deleteOne({ _id: ObjectId(ctx.request.body._id)});
-    
-            ctx.body = result.deletedCount
-    
-        } catch (e) {
-            console.error(e);
-        }
-
-
+        annotations.deleteOne({ _id: ObjectId(ctx.request.body._id) })
+            .then(result => {
+                ctx.body = result.deletedCount
+            })
+            .catch(err => console.error(err.message))
     })
     .get('/annotations/:imageId', async ctx => {
-
         ctx.body = await findOperations({ id: { $regex: `^${ctx.params.imageId}` } });
-
     })
 
-
-
 app.use(router.routes())
+
 app.listen(3001)
