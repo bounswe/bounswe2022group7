655f552bac8fcb11664df579e081cedf42bf8257
CahidArda <57228345+CahidArda@users.noreply.github.com>
2022-12-26 23:42:26 +0300

    Remove tab (FE)

--- frontend/src/pages/ArtItemPage/ArtItemPage.js
+++ frontend/src/pages/ArtItemPage/ArtItemPage.js
@@ -117,83 +117,82 @@ function ArtItemPage(props) {
   } else if (!isLoaded) {
     return <div>Loading...</div>
   } else {
-    return (
-      <GenericCardLayout maxWidth={1000}>
-        <Stack spacing={2} direction="row" justifyContent="space-between" alignItems="center">
+  return (
+    <GenericCardLayout maxWidth={1000}>
+      <Stack spacing={2} direction="row" justifyContent="space-between" alignItems="center">
 
-          <Stack spacing={2} direction="column" justifyContent="start" alignItems="start">
-            <Typography
-              variant="h5"
-              color={theme.palette.primary.main}
-            >
-              Art Item:
-            </Typography>
-            <Typography variant="h4">
-              {artitem.name}
-            </Typography>
-          </Stack>
-
-          <Stack spacing={2} direction="row" justifyContent="end" alignItems="center">
-            {(token) && <IconButton data-testid="bookmarkButton" onClick={handleBookmark} color="secondary"> {bookmarked ? <BookmarkIcon data-testid="bookmarked" /> : <BookmarkBorderOutlinedIcon data-testid="notBookmarked" />}</IconButton>}
-            <CustomizableDropdownMenu data-testid="menuButton" color="secondary" tooltip="More Actions" menuContent={menuContent} />
-          </Stack>
+        <Stack spacing={2} direction="column" justifyContent="start" alignItems="start">
+          <Typography
+            variant="h5"
+            color={theme.palette.primary.main}
+          >
+            Art Item:
+          </Typography>
+          <Typography variant="h4">
+            {artitem.name}
+          </Typography>
         </Stack>
 
-        <ImageDisplay imageId={artitem.imageId} />
+        <Stack spacing={2} direction="row" justifyContent="end" alignItems="center">
+          {(token) && <IconButton data-testid="bookmarkButton" onClick={handleBookmark} color="secondary"> {bookmarked ? <BookmarkIcon data-testid="bookmarked" /> : <BookmarkBorderOutlinedIcon data-testid="notBookmarked" />}</IconButton>}
+          <CustomizableDropdownMenu data-testid="menuButton" color="secondary" tooltip="More Actions" menuContent={menuContent} />
+        </Stack>
+      </Stack>
 
-        <Grid container>
-          <Grid item xs={12} sm={8}>
-            <IconWithText
-              text="Description "
-              variant="h5"
-            />
-            <AnnotatableText text={artitem.description} />
-          </ Grid>
-          <Grid item xs={12} sm={4}>
+      <ImageDisplay imageId={artitem.imageId} />
 
-            <IconWithText
-              icon={<BrushIcon />}
-              text="Creator:"
-              variant="h5"
-            />
-            <UserCard data={artitem.creatorAccountInfo} />
-
-            <IconWithText
-              icon={<LabelIcon />}
-              text="Labels:"
-              variant="h5"
-            />
-            {artitem.labels.join(", ")}
-
-            <IconWithText
-              icon={<CategoryIcon />}
-              text="Categories:"
-              variant="h5"
-            />
-            {artitem.category.join(", ")}
-          </ Grid>
+      <Grid container>
+        <Grid item xs={12} sm={8}>
+          <IconWithText
+            text="Description "
+            variant="h5"
+          />
+          <AnnotatableText text={artitem.description} />
         </ Grid>
-        <AuctionDisplay
-          max_bid={artitem.maxBid}
-          art_item_id={artitem.id}
-          user_id={userData?.id}
-          owner_id={artitem.ownerId}
-          on_auction={artitem.onAuction}
-        />
-        <CommentSection
-          contentId={id}
-          commentList={artitem.commentList}
-        />
-        {reportOpen && <CopyrightReporter
-          id={id}
-          onResponse={() => {
-            props.onResponse("success", "Report submitted");
-            setReportOpen(false);
-          }} />}
-      </GenericCardLayout>
+        <Grid item xs={12} sm={4}>
 
-    )
-  }
+          <IconWithText
+            icon={<BrushIcon />}
+            text="Creator:"
+            variant="h5"
+          />
+          <UserCard data={artitem.creatorAccountInfo} />
+
+          <IconWithText
+            icon={<LabelIcon />}
+            text="Labels:"
+            variant="h5"
+          />
+          {artitem.labels.join(", ")}
+
+          <IconWithText
+            icon={<CategoryIcon />}
+            text="Categories:"
+            variant="h5"
+          />
+          {artitem.category.join(", ")}
+        </ Grid>
+      </ Grid>
+      <AuctionDisplay
+        max_bid={artitem.maxBid}
+        art_item_id={artitem.id}
+        user_id={userData?.id}
+        owner_id={artitem.ownerId}
+        on_auction={artitem.onAuction}
+      />
+      <CommentSection
+        contentId={id}
+        commentList={artitem.commentList}
+      />
+      {reportOpen && <CopyrightReporter
+        id={id}
+        onResponse={() => {
+          props.onResponse("success", "Report submitted");
+          setReportOpen(false);
+        }} />}
+    </GenericCardLayout>
+
+  )}
 }
 
 
