6d6491f2c488e8e890faf2d9aa745fc84dd32f2c
guneyizol <guneyizol@gmail.com>
2022-12-27 11:06:40 +0300

    fıx text annotation bug

--- frontend/src/components/AnnotatableText.js
+++ frontend/src/components/AnnotatableText.js
@@ -1,58 +1,62 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 
 import { Recogito } from '@recogito/recogito-js';
 import '@recogito/recogito-js/dist/recogito.min.css';
 
 function AnnotatableText(props) {
-
   const textEl = useRef(null)
+  
 
-  if (textEl.current) {
-    const recogito = new Recogito({
-      content: textEl.current,
-      widgets: [
-        'COMMENT'
-      ]
-    })
+  useEffect(() => {
+    let recogito
+    
+    if (textEl.current) {
+      recogito = new Recogito({
+        content: textEl.current,
+        widgets: [
+          'COMMENT'
+        ]
+      })
 
-    recogito.loadAnnotations(`/annotations/${props.contentType}${props.id}`)
+      recogito.loadAnnotations(`/annotations/${props.contentType}${props.id}`)
 
-    recogito.on('createAnnotation', annotation => {
-      annotation.id = props.contentType + props.id + '-' + annotation.id
-      console.log(annotation)
-      fetch('/annotations',
-        {
-          method: 'POST',
-          body: JSON.stringify(annotation),
-          headers: {
-            'Content-Type': 'application/ld+json'
-          }
-        })
-    })
+      recogito.on('createAnnotation', annotation => {
+        annotation.id = props.contentType + props.id + '-' + annotation.id
+        console.log(annotation)
+        fetch('/annotations',
+          {
+            method: 'POST',
+            body: JSON.stringify(annotation),
+            headers: {
+              'Content-Type': 'application/ld+json'
+            }
+          })
+      })
 
-    recogito.on('updateAnnotation', annotation => {
-      console.log(annotation)
-      fetch('/annotations',
-        {
-          method: 'PUT',
-          body: JSON.stringify(annotation),
-          headers: {
-            'Content-Type': 'application/ld+json'
-          }
-        })
-    })
+      recogito.on('updateAnnotation', annotation => {
+        console.log(annotation)
+        fetch('/annotations',
+          {
+            method: 'PUT',
+            body: JSON.stringify(annotation),
+            headers: {
+              'Content-Type': 'application/ld+json'
+            }
+          })
+      })
 
-    recogito.on('deleteAnnotation', annotation => {
-      fetch('/annotations',
-        {
-          method: 'DELETE',
-          body: JSON.stringify(annotation),
-          headers: {
-            'Content-Type': 'application/ld+json'
-          }
-        })
-    })
-  }
+      recogito.on('deleteAnnotation', annotation => {
+        fetch('/annotations',
+          {
+            method: 'DELETE',
+            body: JSON.stringify(annotation),
+            headers: {
+              'Content-Type': 'application/ld+json'
+            }
+          })
+      })
+    }
+  }, [])
 
   return (
     <p ref={textEl}>
@@ -60,5 +64,6 @@ function AnnotatableText(props) {
     </p>
   )
 }
+
 
 export default AnnotatableText
